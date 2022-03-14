4eb8d280a92e116004c23ca231f6d1afe93652c5
Samia-Abdella <samutibaba@gmail.com>
2022-03-14 13:47:27 -0700

    Ignore e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 /*
- * main - prints the alphabets in lower case except for 'q' and 'e'
+ * main : prints the alphabets in lower case except for 'q' and 'e'
  * Return - 0 (Always Successful)
  */
+
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (c == 'e' || c == 'q')
-			continue;
+		if (c != 'e' && c != 'q')
+			putchar(c);
 	}
 	putchar('\n');
 	return (0);
