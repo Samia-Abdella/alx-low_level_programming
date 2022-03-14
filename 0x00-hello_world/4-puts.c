aa20a1c1f59d8b272ab07ce103e0457e663985c3
Samia-Abdella <samutibaba@gmail.com>
2022-03-14 08:16:43 -0700

    ADD: Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point prints the statement in quotes
+ *
+ * Return:  a zero if exit is successful, non-zero otherwise
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }	
