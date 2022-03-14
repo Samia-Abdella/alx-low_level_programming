f77e2fd9defd1370e2744acdd3d5cbe75963f395
Samia-Abdella <samutibaba@gmail.com>
2022-03-14 11:27:27 -0700

    ADD:UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point prints the statement in quotes to the standard error
+ *
+ * Return: The program returns a one
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
