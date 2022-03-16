bccaea3d11b51577308d5ff6641bb71f8c6816f3
Samia-Abdella <samutibaba@gmail.com>
2022-03-16 01:18:12 -0700

    Commit 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,14 @@
-#include "_putchar.c"
-#include <string.h>
+#include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints _putchar followed by a new line
  * Return: 0 (Always Successful)
  */
 
-int _putchar(char c);
-
-char* printing (char* string)
-{
-	char* result;
-	for (int c; c < strlen(string); c++)
-	{
-		result = _putchar(c);
-	}
-	result = result + _putchar('\n');
-	return (result);
-}
 
 int main(void)
 {
-	printing("_putchar");
+	printf("_putchar\n");
 	return (0);
 }
