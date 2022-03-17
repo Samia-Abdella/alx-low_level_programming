6218083eaf28cbf9f90676f7cfa55f42bd6f2bb0
Samia-Abdella <samutibaba@gmail.com>
2022-03-17 05:25:54 -0700

    Checks the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the parameter to be checked
- * Return: 0 
+ * Return: 0
  */
 
 int print_sign(int n)
@@ -13,7 +13,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0) 
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
