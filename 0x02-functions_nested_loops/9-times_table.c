deba7dc72aef33949e0bec5cdcf1f8ec1fad264e
Samia-Abdella <samutibaba@gmail.com>
2022-03-17 20:39:00 -0700

    Prints the tome table from 0 to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/* times_table - prints the time table from 0 to 9
+ * Return: nothing
+ */
+
 void times_table(void)
 {
 	int i, j;
@@ -9,9 +13,20 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			int product = i * j;
-			_putchar(product % 10);
-			_putchar(product / 10);
+
+			_putchar(',');
 			_putchar(' ');
+
+			if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(product);
+			}
+			else
+			{
+				_putchar(product / 10);
+				_putchar(product % 10);
+			}
 			_putchar('\n');
 		}
 	}
