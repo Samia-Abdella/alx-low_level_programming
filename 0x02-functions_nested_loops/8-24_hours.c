e4ea9b0d28237163a75ab2ecbb53eaa5a51bd32c
Samia-Abdella <samutibaba@gmail.com>
2022-03-17 20:08:22 -0700

    Prints every minute of the hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute od an hour
+ * Return: nothing to return
+ */
+
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			printf ("%d : %d\n", hours, minutes);
+			minutes++;
+		}
+		hours++;
+		minutes = 0;
+	}
+}
