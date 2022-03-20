efd28afa327eb5f35a1819325f5d38cb15161172
Samia-Abdella <samutibaba@gmail.com>
2022-03-20 07:04:33 -0700

    Returns the sum of the given numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,13 @@
 
 /**
  * add - adds two given numbers
- * Result: returns the sum of the numbers
+ * @a and @b: numbers to be added
+ * Return: the sum of the numbers
  */
 
 int add(int a, int b)
 {
 	int sum = a + b;
+
 	return (sum);
 }
