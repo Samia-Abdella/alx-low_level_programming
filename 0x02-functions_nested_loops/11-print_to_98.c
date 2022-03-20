8cc2735f4b66880ececefdea8210fe07485f3988
Samia-Abdella <samutibaba@gmail.com>
2022-03-20 07:37:33 -0700

    Prints all naturals numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,14 +2,17 @@
 
 /**
  * print_to_98 - prints numbers from n to 98
+ * @n: paramater to be added
  * Return:  nothing
  */
 
 void print_to_98(int n)
 {
+	int i;
+
 	if (n < 98)
 	{
-		for (int i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			_putchar(i);
 		}
