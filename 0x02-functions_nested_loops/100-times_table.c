1e7a29126c406c72ff66293579b1a1f0af91b034
Samia-Abdella <samutibaba@gmail.com>
2022-03-24 01:54:14 -0700

    Prints the time table for a given number

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the time table of the given number
+ * @n: the value to get the time table for
+ */
+
+void print_times_table (int n)
+{
+	int i, j, k;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			printf("%d ", k);
+		}
+	}
+	_putchar('\n');
+}
