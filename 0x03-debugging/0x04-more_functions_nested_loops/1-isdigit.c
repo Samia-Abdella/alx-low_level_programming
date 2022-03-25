e9fc77e65697924935354803145d3185e358908f
Samia-Abdella <samutibaba@gmail.com>
2022-03-25 09:04:47 -0700

    More functions, more nested loops

---
+++ 0x03-debugging/0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: the number to be checked
+ * Return: 1 if c is a digit and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
---
+++ 0x03-debugging/0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: returns their product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
